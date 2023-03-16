dfb6379f1984b70181dd6bd854dd2a7a27f92640
Rafael <rafaelreinertd@hotmail.com>
2023-03-16 15:21:36 -0300

    Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
 import { Link } from "react-router-dom";
 import styled from "styled-components"
 
 export default function Footer() {
-    const percentage = 30;
+    // eslint-disable-next-line
+    const [percentage, setPercentage] = useState(40);
 
-    function progressStyle(){
+    function progressStyle() {
         return buildStyles({
-            
+            backgroundColor: "#52B6FF",
+            textColor: "#FFFFFF",
+            pathColor: "#FFFFFF",
+            trailColor: "transparent",
+            // rotation: -(percentage/100)/2
         })
     }
 
     return (
         <Foot>
-            <div>
-                <CircularProgressbar value={percentage} styles={progressStyle} text={<Link to='/hoje' >Hoje</Link>} />
-            </div>
+            <Link to='/habitos' >Hábitos</Link>
+            <Link to='/hoje' >
+                <div>
+                    <CircularProgressbar
+                        value={percentage}
+                        styles={progressStyle()}
+                        background
+                        backgroundPadding={6}
+                        text={'Hoje'} />
+                </div>
+            </Link>
+            <Link to='/historico' >Histórico</Link>
         </Foot>
     )
 }
@@ -28,16 +44,21 @@ const Foot = styled.footer`
     width: 100%;
     height: 70px;
     background-color: #FFFFFF;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
     box-shadow: 0 -5px 10px rgba(0, 0, 0, .15);
 
     div{
-        height: 50px;
-        width: 50px;
+        height: 90px;
+        width: 90px;
         position: relative;
         top: -20px;
-        border-radius: 100%;
-        background-color: aqua;
+    }
+
+    a{
+        font-family: 'Lexend Deca';
+        font-size: 20px;
+        color: #52B6FF;
+        text-decoration: none;
     }
 `;
